04073bdfbdf9ad3567bff518531533199ef9e7c7
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2018-12-27 19:08:02 +0100

    Pressing support

--- QmuTactile.cpp
+++ QmuTactile.cpp
@@ -8,26 +8,33 @@ QmuTactile::QmuTactile(uint8_t pin) {
 void QmuTactile::loop(void) {
 
     uint8_t pinState = digitalRead(_pin);
+    _state = TACTILE_STATE_NONE;
 
     //Press moment
     if (pinState == LOW && _previousPinState == HIGH) {
         _pressMillis = millis();
+        _nextPressingEvent = 0;
+    }
+
+    const uint32_t buttonTime = abs(millis() - _pressMillis);
+
+    //Pressing
+    if (pinState == LOW && buttonTime > TACTILE_PRESSING_TIME) {
+        if (millis() > _nextPressingEvent) {
+            _state = TACTILE_STATE_PRESSING;
+            _nextPressingEvent = millis() + 500;
+        }
     }
 
     //Release moment
     if (pinState == HIGH && _previousPinState == LOW) {
-        const uint32_t buttonTime = abs(millis() - _pressMillis);
 
         if (buttonTime > TACTILE_LONG_PRESS_TIME) {
             _state = TACTILE_STATE_LONG_PRESS;
         } else if (buttonTime > TACTILE_MIN_PRESS_TIME) {
             _state = TACTILE_STATE_SHORT_PRESS;
-        } else {
-            _state = TACTILE_STATE_NONE;
         }
 
-    } else {
-        _state = TACTILE_STATE_NONE;
     }
 
     _previousPinState = pinState;
--- QmuTactile.h
+++ QmuTactile.h
@@ -8,11 +8,13 @@
 enum tactileStateFlags {
     TACTILE_STATE_NONE,
     TACTILE_STATE_SHORT_PRESS,
-    TACTILE_STATE_LONG_PRESS
+    TACTILE_STATE_LONG_PRESS,
+    TACTILE_STATE_PRESSING
 };
 
 #define TACTILE_MIN_PRESS_TIME 50
 #define TACTILE_LONG_PRESS_TIME 1000
+#define TACTILE_PRESSING_TIME 400
 
 class QmuTactile {
   public:
@@ -25,6 +27,7 @@ class QmuTactile {
     uint8_t _previousPinState = HIGH;
     uint32_t _pressMillis = 0;
     uint8_t _state = TACTILE_STATE_NONE;
+    uint32_t _nextPressingEvent = 0;
 };
 
 #endif
